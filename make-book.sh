e1fcb0c1135e6e1cfa561a839736ee26ff02fd98
Nathanael Meister <29178298+nathanaelmeister@users.noreply.github.com>
2019-12-02 22:52:33 +0100

    enhanced versions

--- make-book.sh
+++ make-book.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-#folder="$1"
+# run in parent directory. And select ${dir} directory
+# like: `bash script.sh ${dir}-dir`
+
+dir="$1"
 bookname="Sebastian_Lee_-_Op_70_-_40_easy_etudes.ly"
-book=$(sed 's/\///g' <<< "$bookdir")
+
+# parent=$(pwd | awk -F'/' '{print $NF}')
+part=$( awk -F'-' '{print $NF}' <<< "$dir" |
+        sed 's/\///g')
 
 cat << EOF > "$bookname"
 \version "2.18.2"
@@ -14,12 +20,52 @@ cat << EOF > "$bookname"
 
 \include "./global-files/paper.ily"
 \include "./global-files/pdf-header.ily"
+\include "./global-files/tuplet-bow.ily"
 
 \book {
+
+  \include "./global-files/Nr_00-Frontpage.ily"
 EOF
 
-for file in ./input-files/*.ily; do
-    echo "  \include \"$file\""
+for file in ./${dir}/Nr_{01..40}-${part}.ily; do
+
+  getnum=$(awk -F'_' '{print $2}' <<< "$file" |
+             awk -F'.' '{print $1}' |
+             awk -F'-' '{print $1}' |
+             sed 's/^0//g' )
+
+
+  # check if tuplet-bow is used and create `\include` line
+  # and `\layout {}` if true
+
+  tbincl=$(grep "TupletBracket" "$file")
+
+  if [ -n "$tbincl" ]; then
+
+    tblayout=$(cat <<-EOF
+	\layout {
+      \printTupletBow
+    }
+	EOF
+    )
+  else
+    tblayout="\layout {}"
+  fi
+
+  cat << EOF
+
+  %%%% Score Number: $getnum ==================================%%%%
+
+  \score {
+    \new StaffGroup = "" \with {
+      instrumentName = \markup { \bold \huge { \larger "${getnum}." }}}
+    <<
+      \new Staff = "celloI" \with { midiInstrument = #"cello" }
+      \include "${file}"
+    >>
+    $tblayout
+  }
+EOF
 done >> "$bookname"
 
 echo "}" >> "$bookname"
--- make-single-pages.sh
+++ make-single-pages.sh
@@ -6,9 +6,36 @@
 for file in *.ily; do
 
   getnum=$(awk -F'_' '{print $2}' <<< "$file" |
-             awk -F'.' '{print $1}' | sed 's/^0//g')
+             awk -F'.' '{print $1}' |
+             awk -F'-' '{print $1}' |
+             sed 's/^0//g' )
 
-  filepath="../single-pages/${file}"
+  parent=$(pwd | awk -F'/' '{print $NF}')
+  part=$(awk -F'-' '{print $NF}' <<< "$parent")
+
+  chname=${file//.ily/.ly}
+  filepath="../single-pages-${part}/Sebastian_Lee_-_Op_70_-_${chname}"
+
+  # check if tuplet-bow is used and create `\include` line
+  # and `\layout {}` if true
+
+  tbincl=$(grep "TupletBracket" "$file")
+
+  if [ -n "$tbincl" ]; then
+
+    tblink='\include "../global-files/tuplet-bow.ily"'
+
+    tblayout=$(cat <<-EOF
+	\layout {
+	    \printTupletBow
+	  }
+	EOF
+    )
+  else
+    tblayout="\layout {}"
+  fi
+
+  # print file:
 
   cat <<-EOF > "$filepath"
 	\version "2.18.2"
@@ -20,9 +47,28 @@ for file in *.ily; do
 
 	\include "../global-files/gitlink.ily"
 	\include "../global-files/header.ily"
+	$tblink
 
-	\include "../input-files/${file}"
+	\score {
+	  \new StaffGroup = "" \with {
+	    instrumentName = \markup { \bold \huge { \larger "${getnum}." }}
+	  }
+	  <<
+	    \new Staff = "celloI" \with { midiInstrument = #"cello" }
+
+	    \include "../${parent}/${file}"
+
+	  >>
+	  $tblayout
+	  \midi {}
+	  \header {
+	    composer = "Sebastian Lee"
+	    %opus = "Op. 70"
+	  }
+	}
 
 	EOF
 
+  # remove multible blank lines if needed:
+  sed -i 'N;/^\n$/D;P;D;' "$filepath"
 done
