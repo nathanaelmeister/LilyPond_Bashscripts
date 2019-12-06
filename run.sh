0afc9143ce822f055579fe7f6853ac2bc023aac6
Nathanael Meister <29178298+nathanaelmeister@users.noreply.github.com>
2019-12-06 02:32:14 +0100

    contains some major fixes and improvements
    - first of all it works now. nice :-)
    - second it opens nano for a quick check
      this is also needed because there is no input for time signature and key
      - this could be added to the infile
    
    - major improvements to the AddBarNumbers.sh script should be done in any way
      - if this script could ignore lines starting with certain charachters it was
        possible to add more to the basic input, but this is a topic to be solved
        on a different place at a differemnt time with a different (yea I sometimes like writing).

--- run.sh
+++ run.sh
@@ -10,26 +10,47 @@
 
 nn="$1"
 
+if [ -z "$nn" ]; then
+
+  echo " no input number supplied!"
+  echo " ==> quitting, without nothing done :-("
+  exit
+
+fi
+
 cd _WD/ || exit
 bash "$HOME/Dokumente/Command-Line/MyScripts/Lilypond/Bar-Numbers/simpler-4.sh"
 
-mv ./infile.ily "../input-files-celloII/Nr_${nn}-celloII.ily"
+mv -i ./infile.ily "../input-files-celloII/Nr_${nn}-celloII.ily"
 
-cd ./input-files-celloII/ || exit
+cd ../input-files-celloII/ || exit
 
-bash ../_scripts/make-single-pages.sh
-bash ../make-two-systems.sh
+bash ../_scripts/make-single-pages.sh "Nr_${nn}-celloII.ily"
+bash ../_scripts/make-two-systems.sh "Nr_${nn}-celloII.ily"
+
+# added a check on input file; this higly needed ========#
+# to enter time signature and key:                       #
+nano -ET2 "../input-files-celloII/Nr_${nn}-celloII.ily"  #
+#========================================================#
 
 cd ../single-pages-celloI_and_II/ || exit
 
-lilypond "./S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.ly"
+if file "S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.pdf"; then
 
-if file "./S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.pdf"; then
+  lilypond "S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.ly"
 
-  echo "file: ./S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.pdf is present :-)"
+else
+  echo "no LilyPond file!"
+fi
 
-  xpdf "./S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.pdf" &
+if file "S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.pdf"; then
+
+  echo "file: S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.pdf is present :-)"
+
+  xpdf "S_Lee_-_Op_70_-_I_u_II_Nr_${nn}.pdf" &
   echo "opening..."
 
+else
+  echo "no pdf file!"
   exit
 fi
